30dfdfa972e79cfd6dad8a4847a8797b9a39692e
bholzmmc <brian.m.holzman@mmc.com>
2018-03-14 13:59:14 +0000

    fixed issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [{
+  'hostname' => 'ec2-184-73-128-29.compute-1.amazonaws.com',
+  'ipaddress' => '184.73.128.29',
+  'port' => 80,
+  'ssl_port' => 80
+}]
+include_recipe 'haproxy::manual'
