2eb53343228e9c6b3d3e44cd4e5583c31d5bc960
bholzmmc <brian.m.holzman@mmc.com>
2018-03-14 14:55:38 +0000

    adding apache node to lb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-node.default['haproxy']['members'] = [{
-  'hostname' => 'ec2-184-73-128-29.compute-1.amazonaws.com',
+node.default['haproxy']['members'] = [
+  { 'hostname' => 'ec2-184-73-128-29.compute-1.amazonaws.com',
   'ipaddress' => '184.73.128.29',
   'port' => 80,
-  'ssl_port' => 80
-}]
+  'ssl_port' => 80 } ,
+  { 'hostname' => 'ec2-54-81-33-138.compute-1.amazonaws.com',
+  'ipaddress' => '54.81.33.138',
+  'port' => 80,
+  'ssl_port' => 80 }
+]
 include_recipe 'haproxy::manual'
