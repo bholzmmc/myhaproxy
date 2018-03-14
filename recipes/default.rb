ba1bb35801270fe35a9304ed5d1058033a318362
bholzmmc <brian.m.holzman@mmc.com>
2018-03-14 18:22:39 +0000

    lab to dynamically identify web servers using node attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,14 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-node.default['haproxy']['members'] = [
-  { 'hostname' => 'ec2-184-73-128-29.compute-1.amazonaws.com',
-  'ipaddress' => '184.73.128.29',
-  'port' => 80,
-  'ssl_port' => 80 } ,
-  { 'hostname' => 'ec2-54-81-33-138.compute-1.amazonaws.com',
-  'ipaddress' => '54.81.33.138',
-  'port' => 80,
-  'ssl_port' => 80 }
-]
+all_web_nodes = search('node','role:web_server')
+members = []
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname'  => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+  members.push(members)
+end
+
+node.default['haproxy']['members'] = members
+
 include_recipe 'haproxy::manual'
